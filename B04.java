a6824509af9e30fa21a78b1377f7818172f88b15
wnsxo11 <cat5670@naver.com>
2016-10-07 18:10:44 +0900

    원격에서 commit

---
+++ B04.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class B04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner ss = new Scanner(System.in);
+		System.out.print("연봉을 원단위로 입력하세요  ");
+		ss.nextInt();
+		
+		
+	}
+
+}
